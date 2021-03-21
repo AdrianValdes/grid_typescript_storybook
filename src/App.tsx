ca0e045058189915a319a79d47b9f4312990a6dc
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-21 18:28:04 +0100

    feature: add justify-content and align-items

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,25 @@ function App() {
   return (
     <div className='App'>
       <GridRuler spacing='sm' />
-      <Grid container spacing='md'>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+      <Grid
+        container
+        spacing='md'
+        justifyContent='flex-end'
+        alignItems='center'
+      >
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 1</Box>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 2</Box>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 3</Box>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 3</Box>
         </Grid>
       </Grid>
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,9 +1,11 @@
-import React, { ReactNode } from 'react';
+import React, { CSSProperties, ReactNode } from 'react';
 import styles from './Grid.module.scss';
 import cn from 'classnames';
 
 type Cols = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 type Spacing = 'sm' | 'md' | 'lg';
+type JustifyContent = 'flex-start' | 'center' | 'flex-end' | 'space-between';
+type AlignItems = 'flex-start' | 'center' | 'flex-end';
 
 type PropTypes = {
   children: ReactNode;
@@ -14,6 +16,9 @@ type PropTypes = {
   md?: Cols;
   lg?: Cols;
   spacing?: Spacing;
+  justifyContent?: JustifyContent;
+  alignItems?: AlignItems;
+  style?: CSSProperties;
 };
 
 const Grid = ({
@@ -25,6 +30,9 @@ const Grid = ({
   md,
   lg,
   spacing,
+  justifyContent,
+  alignItems,
+  ...style
 }: PropTypes): JSX.Element => {
   const classNames = cn({
     [styles.Grid_container]: container,
@@ -34,8 +42,14 @@ const Grid = ({
     [styles[`Grid_md_${md}`]]: md,
     [styles[`Grid_lg_${lg}`]]: lg,
     [styles[`Grid_spacing_${spacing}`]]: spacing,
+    [styles[`Grid_justifyContent_${justifyContent}`]]: justifyContent,
+    [styles[`Grid_alignItems_${alignItems}`]]: alignItems,
   });
-  return <div className={classNames}>{children}</div>;
+  return (
+    <div className={classNames} {...style}>
+      {children}
+    </div>
+  );
 };
 
 export default Grid;
