0a73d2340ee788c52f7079ddf483c295e846ce0b
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-21 23:21:01 +0100

    add controls

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,23 @@ import { GridRuler } from './components/GridRuler/GridRuler';
 function App() {
   return (
     <div className='App'>
-      <GridRuler spacing='sm' />
-      <Grid container spacing='md' justifyContent='flex-end'>
-        <Grid item xs={1} sm={6} md={4} lg={3}>
+      <GridRuler spacing='sm'></GridRuler>
+
+      <Grid container spacing='sm'>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
           <Box>Box 1</Box>
         </Grid>
 
-        <Grid item xs={1} sm={6} md={4} lg={3}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
           <Box>Box 2</Box>
         </Grid>
 
-        <Grid item xs={1} sm={6} md={4} lg={3}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
           <Box>Box 3</Box>
         </Grid>
 
-        <Grid item xs={1} sm={6} md={4} lg={3}>
-          <Box>Box 3</Box>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Box>Box 4</Box>
         </Grid>
       </Grid>
     </div>
